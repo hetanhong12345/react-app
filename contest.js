7c83b4b6431bbfd78c8242e293b15637cd7df5e8
何坦宏 <hetanhong@mofanghr.com>
2018-05-14 14:46:02 +0800

    contest 84

--- contest.js
+++ contest.js
@@ -46,3 +46,75 @@ let consecutiveNumbersSum = function (N) {
 };
 
 consecutiveNumbersSum(9);
+
+
+let validTicTacToe = function (board) {
+    let mapstrs = [[], [], []];
+    for (let i = 0; i < 3; i++) {
+        mapstrs[0][i] = board[0][i];
+        mapstrs[1][i] = board[1][i];
+        mapstrs[2][i] = board[2][i];
+    }
+    let result = false;
+    deep('X');
+    return result;
+
+    function deep(char) {
+        if (isFull()) {
+            result = true;
+            return;
+        }
+        if (isWin()) {
+            return;
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (mapstrs[i][j] === ' ') {
+                    mapstrs[i][j] = char;
+                    if (char === 'X') {
+                        deep('O')
+                    } else {
+                        deep('X')
+                    }
+                    mapstrs[i][j] = ' ';
+                }
+
+            }
+        }
+
+    }
+
+    function isWin() {
+        if (mapstrs[0][0] !== ' ' && mapstrs[0][0] === mapstrs[1][1] && mapstrs[0][0] === mapstrs[2][2]) {
+            return true;
+        }
+
+        if (mapstrs[2][0] !== ' ' && mapstrs[2][0] === mapstrs[1][1] && mapstrs[2][0] === mapstrs[0][2]) {
+            return true;
+        }
+        for (let i = 0; i < 3; i++) {
+            if (mapstrs[i][0] !== ' ' && mapstrs[i][0] === mapstrs[i][1] && mapstrs[i][0] === mapstrs[i][2]) {
+                return true;
+            }
+            if (mapstrs[0][i] !== ' ' && mapstrs[0][i] === mapstrs[1][i] && mapstrs[0][i] === mapstrs[2][i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function isFull() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (mapstrs[i][j] === ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+};
+console.log(validTicTacToe(["   ","   ","   "]));
